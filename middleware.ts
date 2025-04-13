e71eae4db60cf48414785730cdb71c87699eefa6
My <yy_ltd@163.com>
2025-04-13 20:29:02 +0800

    移除中间件以解决执行失败错误

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-// 简化的中间件，不执行任何认证
-export function middleware() {
-  // 不执行任何操作，只是通过请求
-  return
-}
-
-export const config = {
-  matcher: ["/((?!api/trpc|_next/static|_next/image|favicon.ico).*)"],
-}
